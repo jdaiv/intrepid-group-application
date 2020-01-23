6a8c5cff2c05825bb2b3b10b2bc90187203219b6
John Aivaliotis <johnaivaliotis@protonmail.com>
2020-01-23 14:32:47 +1100

    Task 3 done.

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@ new Vue({
     el: '#app',
     data: {
         books: [],
+        sortBy: 'year',
+        sort: 'desc'
     },
     created: function() {
         this.books = window.__INITIAL_STATE__
@@ -10,6 +12,24 @@ new Vue({
 
     },
     computed: {
+        // I figure if you ever expanded on this you'd want a good base for sorting.
+        sortedBooks: function() {
+            // We can use Array.from here for a shallow copy since were not
+            // modifying object data.
+            const sortedArray = Array.from(this.books);
 
+            switch(this.sortBy) {
+                case 'year':
+                    sortedArray.sort((a, b) => a.year - b.year);
+                    break;
+            }
+
+            // Sorted by ascending by default.
+            if (this.sort == 'desc') {
+                sortedArray.reverse()
+            }
+
+            return sortedArray;
+        }
     }
 });
